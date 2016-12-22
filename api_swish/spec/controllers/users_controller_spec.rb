cd43c488f68364d9739f775b77e76ff8ef298b92
Sam Rosenshein <srosenshein1@gmail.com>
2016-12-22 00:37:49 +0000

    adds the user create endpoint

--- api_swish/spec/controllers/users_controller_spec.rb
+++ api_swish/spec/controllers/users_controller_spec.rb
@@ -16,5 +16,48 @@ RSpec.describe Api::UsersController, type: :controller do
             expect(response).to have_http_status(:success)
         end
     end
-
+    
+    describe "POST #create" do
+        context "when is successfully created" do
+            it "renders the json representation for the user record" do
+                @user_atts = attributes_for(:user)
+                post :create, params: {user: @user_atts, format: :json}
+                user_response = JSON.parse(response.body, symbolize_names: true)
+                expect(user_response[:email]).to eql(@user_atts[:email])
+            end
+            
+            it "returns http success (201)" do
+                @user_atts = attributes_for(:user)
+                post :create, params: {user: @user_atts, format: :json}
+                expect(response).to have_http_status(201)
+            end
+        end
+        
+        context "when is not successfully created" do
+            # no email 
+            let(:inv_user) do 
+                {
+                    password: "helloworld", 
+                    password_confirmation: "helloworld"
+                }
+            end
+                
+            it "renders an errors json" do
+                post :create, params: {user: inv_user, format: :json}
+                user_response = JSON.parse(response.body, symbolize_names: true)
+                expect(user_response).to have_key(:errors)
+            end
+            
+            it "renders the json errors on why the user could not be created" do
+                post :create, params: {user: inv_user, format: :json}
+                user_response = JSON.parse(response.body, symbolize_names: true)
+                expect(user_response[:errors][:email]).to include "can't be blank"
+            end
+            
+            it "returns http status 422" do
+                post :create, params: {user: inv_user, format: :json}
+                expect(response).to have_http_status(422)
+            end
+        end
+    end
 end
