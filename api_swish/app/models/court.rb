f5a57197094ce2818cb1023838d25cdca0ba081f
Sam Rosenshein <srosenshein1@gmail.com>
2016-12-28 17:39:00 +0000

    adds court data

--- api_swish/app/models/court.rb
+++ api_swish/app/models/court.rb
@@ -1,4 +1,4 @@
 class Court < ApplicationRecord
     geocoded_by :address
-    after_validation :geocode, :if => :address_changed?
+    after_validation :geocode, if: ->(obj){ !obj.latitude.present? or obj.address_changed? }
 end
--- api_swish/db/seeds.rb
+++ api_swish/db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'nokogiri'
+
+f = File.open(File.join(Rails.root, 'db', 'bball_courts.xml'))
+doc = Nokogiri::XML(f)
+f.close
+
+all_courts = []
+
+doc.css('facility').each do |node|
+    children = node.children
+    
+    name = children.css('Name').inner_text
+    address = children.css('Location').inner_text + ', NY, New York'
+    lat = children.css('lat').inner_text
+    long = children.css('lon').inner_text
+    
+    all_courts << {
+        name: name,
+        description: "",
+        status: "",
+        address: address,
+        latitude: lat,
+        longitude: long
+    }
+end
+
+Court.create(all_courts)
